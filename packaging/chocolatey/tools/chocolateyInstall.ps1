aa2541b91ff7e153e9a27108da39a308591a361b
TRGamer-Tech <till18boi@gmail.com>
2026-04-14 18:11:24 +0200

    feat: add package manifests for Scoop, Winget, and Chocolatey

--- packaging/chocolatey/tools/chocolateyInstall.ps1
+++ packaging/chocolatey/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ $packageName   = 'fluenttaskscheduler'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $version       = '1.8.0'
 $url64         = "https://github.com/TRGamer-tech/FluentTaskScheduler/releases/download/v$version/Setup-x64.msi"
-$checksum64    = '<SHA256_OF_Setup-x64.msi>'
+$checksum64    = '8FFB4D207528D4D80F6178CA9A4EC3B0445A43B442E36C6893170CB1D8E30106'
 
 $packageArgs = @{
   packageName    = $packageName
